ad4ef1caeb3a1166d1a5ed5c72ac26d5e061426f
Stas-0k <brickbybrick15@gmail.com>
2022-11-18 14:20:05 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries.js';
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list')
+let arrCountry = [];
+input.addEventListener('input', debounce(inputEvent, DEBOUNCE_DELAY));
+
+function inputEvent(e) {
+  localStorage.setItem('countryName', JSON.stringify(e.target.value));
+  const countryFind = JSON.parse(localStorage.getItem('countryName'));
+   API.fetchCountries(countryFind);
+    
+}
