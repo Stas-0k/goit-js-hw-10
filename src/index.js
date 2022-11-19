84aa4fe55b3ca6a8a18f5cec80ff035344a98b15
Stas-0k <brickbybrick15@gmail.com>
2022-11-20 00:16:32 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,49 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
+import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
 
 input.addEventListener('input', debounce(inputEvent, DEBOUNCE_DELAY));
 
 function inputEvent(e) {
-    fetchCountries(e.target.value)
-    .then((countries) => renderList(countries))
-    
+    fetchCountries(e.target.value.trim())
+        .then((countries) => renderList(countries))
+        
 }
 
 function renderList(countries) { 
     
-        countries.map((country) => { 
-        countryList.innerHTML +=  
-        `<li>
+    if (countries.length > 10) {
+        Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.')
+    } else if (countries.length > 1 & countries.length <= 10) {
+        countries.map((country) => {
+            countryList.innerHTML +=
+                `<li>
             <img src="${country.flags.svg}">
             <p>${country.name.official}</p>
         </li>`
-    })
-    }    
+        })
+    } else if (countries.length === 1) {
+        countries.map((country) => 
+        {
+            countryList.innerHTML = '';
+            countryInfo.innerHTML =
+                `<div>
+            <div class="title-cont">
+            <img src="${country.flags.svg}">
+            <h1 class="country-name">${country.name.official}</h1>
+            </div>
+            <h2 class="cont-data"><p class="main-text">Capital:</p>${country.capital}</h2>
+            <h2 class="cont-data"><p class="main-text">Population:</p>${country.population}</h2>
+            <h2 class="cont-data"><p class="main-text">Languages:</p>${Object.values(country.languages)}</h2>
+        </div>`
+        })
+    } else { 
+        countryList.innerHTML = ''
+        countryInfo.innerHTML = ''
+    }
+    }  
